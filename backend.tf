5463f2e3856970c91f4c0ab30c9100b3092bc2e2
Harsh-Devsecops <149611366+git01h@users.noreply.github.com>
2023-12-22 15:50:15 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,6 +4,6 @@ backend "azurerm" {
     resource_group_name  = "example2"
     storage_account_name = "dso456108"
     container_name       = "stfstate"
-    key                  = "akterraform.tfstate"
+    key                  = "terraform.tfstate"
   }
 }
